75d3321a7c3b71b95e20436099528701786183f7
Carole BRA <carole.bra@orange.com>
2018-07-04 14:22:03 +0200

    Define some variables in my activity

---
+++ MainActivity.java
@@ -0,0 +1,14 @@
+@EActivity(R.layout.activity_main)
+public class MainActivity extends AppComptActivity {
+@ViewById(R.id.mText)
+TextView mTextView;
+
+@ViewById(R.id.mButton)
+TextView mButton;
+
+@AfterViews
+void init(){
+//Must be completed 
+}
+
+}
